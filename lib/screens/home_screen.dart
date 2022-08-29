f8b4406b3289a38e08a521f0486a2f0fe67c2334
Jaime Victoriano <xaxo.victoriano@gmail.com>
2022-08-29 11:08:14 -0400

    class 29/08

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+//   const HomeScreen({Key? key}) : super(key: key);
 
+  final styleTextRow = new TextStyle(fontSize: 32);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,6 +11,26 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Warehouse'),
         backgroundColor: Colors.red,
         leading: Icon(Icons.warehouse),
+      ),
+      body: Column(children: [
+        Row(
+          children: <Widget>[
+            Text('uno', style: styleTextRow),
+            Text(
+              'dos',
+              style: styleTextRow,
+            ),
+            Text('tres', style: styleTextRow),
+          ],
+        )
+      ]),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.check),
+        backgroundColor: Colors.red,
+        onPressed: () {
+          print('HOLA (con hate)');
+        },
       ),
     );
   }
