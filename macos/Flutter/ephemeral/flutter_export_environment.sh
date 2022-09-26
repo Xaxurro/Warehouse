9c1611a2f9b63498c9a5c3da4f9a89edba73ac18
Joaquin Puga <joaquin.puga@usm.cl>
2022-09-26 01:12:27 -0300

    arreglo?

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\tiare\Downloads\flutter_windows_3.3.2-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\tiare\OneDrive\Escritorio\Joaquin\gits\Warehouse"
+export "FLUTTER_ROOT=E:\flutter_windows_3.3.1-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=F:\repos\Warehouse"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
