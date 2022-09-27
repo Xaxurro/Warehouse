404769ba0e8c926a60de20bbc1848d7460dfde7a
AlumnosInf <AlumnosInf@soportetiu.local>
2022-09-27 12:17:21 -0300

    Clase 27-09

--- lib/tabs/addItem.dart
+++ lib/tabs/addItem.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const addItem());
-
 class addItem extends StatelessWidget {
-  const addItem({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          leading :IconButton(icon: Icon(Icons.arrow_back_ios_new,color: Colors.white,),onPressed: () {
-            Navigator.pop(context);
-          },) ,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios_new,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
           title: const Text('Add item'),
           centerTitle: true,
           backgroundColor: Colors.redAccent.shade700,
--- lib/tabs/tab2.dart
+++ lib/tabs/tab2.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:warehouse/tabs/addItem.dart';
 
 class Tab2Page extends StatelessWidget {
-  const Tab2Page({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: const Center(
-          child: Text('Tab 2', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+          child: Text('Tab 2',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(context,MaterialPageRoute(builder: (context)=> const addItem()),);},
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => addItem()),
+          );
+        },
         child: const Icon(Icons.add),
         backgroundColor: Colors.redAccent.shade700,
       ),
