cba8d35b16085cacaaacabc7e55c177a27428e56
AlumnosInf <AlumnosInf@soportetiu.local>
2022-10-04 12:41:31 -0300

    Certamen 1

--- certamen_1_jaime_victoriano/android/app/src/main/kotlin/com/example/certamen_1_jaime_victoriano/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.certamen_1_jaime_victoriano
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- certamen_1_jaime_victoriano/lib/style.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-TextStyle Title = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
-TextStyle SubTitle = TextStyle(fontSize: 4);
