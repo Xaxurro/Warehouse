6156724519688b144d0634ac4e453b4da842fbf9
Xaxurro <78761239+Xaxurro@users.noreply.github.com>
2022-11-26 01:28:41 -0300

    Formulario Warehouse casi terminado

--- lib/Functions/FormFunctions.dart
+++ lib/Functions/FormFunctions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:warehouse/globals.dart' as globals;
 
 Widget FormPadding(Widget input) =>
     Padding(
@@ -16,6 +17,9 @@ Widget FormValidation(BuildContext context, GlobalKey<FormState> formKey) => For
         }
       },
       child: const Text('Submit'),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: globals.mainColor
+      ),
     ),
 );
 
@@ -26,5 +30,66 @@ Widget InputText(String label, TextEditingController controller, String? Functio
       labelText: label
     ),
     validator: validation,
+  ),
+);
+
+Widget InputTextArea(String label, TextEditingController controller, String? Function(String?)? validation) => FormPadding(
+  TextFormField(
+    decoration: InputDecoration(
+      border: OutlineInputBorder(),
+      labelText: label
+    ),
+    validator: validation,
+    maxLines: null,
+  ),
+);
+
+Widget InputNumber(String label, TextEditingController controller, String? Function(String?)? validation) => FormPadding(
+  TextFormField(
+    decoration: InputDecoration(
+      border: OutlineInputBorder(),
+      labelText: label,
+    ),
+    keyboardType: TextInputType.number,
+    validator: validation,
+  ),
+);
+
+Widget InputBoolean(String label, bool state, void Function(bool?) change) => FormPadding(
+  Row(
+    children: [
+      SizedBox(
+        width: 10,
+        child: Checkbox(
+          value: state,
+          activeColor: globals.mainColor,
+          onChanged: change,
+        ),
+      ),
+      SizedBox(width: 10.0),
+      Text(label)
+    ]
+  )
+);
+
+Widget InputSelection(String label, BuildContext context, List<ListTile> list) => FormPadding(
+  ElevatedButton(
+    child: Text(label),
+    onPressed: (() {
+      showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            child: Column(
+              children: list,
+            ),
+            height: list.length * 60,
+          );
+        }
+      );
+    }),
+    style: ElevatedButton.styleFrom(
+      backgroundColor: globals.mainColor
+    ),
   ),
 );
--- lib/pages/bodies/AddItemForm.dart
+++ lib/pages/bodies/AddItemForm.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:warehouse/Functions/FormFunctions.dart';
 
@@ -12,51 +13,99 @@ class AddItemForm extends StatefulWidget {
 
 class AddItemFormState extends State<AddItemForm> {
   final _formKey = GlobalKey<FormState>();
+
   final ItemName = TextEditingController();
+  final ItemStock = TextEditingController();
+  final ItemDescription = TextEditingController();
+  String ItemState = "Null";
+  String ItemCategory = "Null";
+  bool favourite = false;
+
 
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          InputText("Name", ItemName, (value) {
-            if (value == null || value.isEmpty) {
-              return "Enter Text";
-            }
-            return null;
-          }),
-
-          //Stock
-          InputText("Stock", ItemName, (value) {
-            if (value == null || value.isEmpty) {
-              return "Enter Text";
-            }
-            return null;
-          }),
-
-          //Estado
-          InputText("Name", ItemName, (value) {
-            if (value == null || value.isEmpty) {
-              return "Enter Text";
-            }
-            return null;
-          }),
-
-          //Favorito
-          InputText("Name", ItemName, (value) {
-            if (value == null || value.isEmpty) {
-              return "Enter Text";
-            }
-            return null;
-          }),
-          
-          //Categoria
-          //Descripcion
-          FormValidation(context, _formKey)
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Nombre
+            InputText("Name", ItemName, (value) {
+              if (value == null || value.isEmpty) {
+                return "Enter Name";
+              }
+              return null;
+            }),
+      
+            //Stock
+            InputNumber("Stock", ItemStock, (value) {
+              if (value == null || value.isEmpty) {
+                return "Enter Stock";
+              }
+              return null;
+            }),
+      
+            //Estado
+            InputSelection(
+              "State",
+              context,
+              [
+                ListTile(
+                  leading: Icon(Icons.warehouse),
+                  title: Text("Warehouse"),
+                  onTap: () => selectItem("Warehouse"),
+                ),
+                ListTile(
+                  leading: Icon(CupertinoIcons.car),
+                  title: Text("Terrain"),
+                  onTap: () => selectItem("Terrain"),
+                ),
+              ]
+            ),
+            
+            //Categoria
+            InputSelection(
+              "Category",
+              context,
+              [
+                ListTile(
+                  title: Text("test"),
+                  onTap: () => selectItem("test"),
+                ),
+              ]
+            ),
+      
+            //Favorito
+            InputBoolean("Favourite", favourite, (value) {
+                setState(() {
+                  favourite = !favourite;
+                });
+            }),
+      
+            //Descripcion
+            InputTextArea("Description (Optional)", ItemDescription, (value) {return null;}),
+      
+            FormValidation(context, _formKey)
+          ],
+        ),
       ),
     );
+  }
+
+  void selectItem(String state) {
+    Navigator.pop(context);
+    setState(() {
+      ItemState = state;
+      print(ItemState);
+    });
+  }
+
+  void selectCategory(String category) {
+    Navigator.pop(context);
+    setState(() {
+      ItemCategory = category;
+      print(ItemCategory);
+    });
   }
 }
