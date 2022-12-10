82dae4379bc194aa855e51396bcd622b76b59ad6
Xaxurro <78761239+Xaxurro@users.noreply.github.com>
2022-12-10 19:22:55 -0300

    FUCK FLUTTER

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:warehouse/pages/BottomTabBar';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+import 'package:warehouse/globals.dart' as globals;
+import 'package:warehouse/pages/Dashboard.dart';
+import 'package:warehouse/pages/Warehouse.dart';
+import 'package:warehouse/pages/Locations.dart';
+import 'package:warehouse/pages/Profile.dart';
+void main() {
   runApp(const MyApp());
 }
 
@@ -16,5 +20,56 @@ class MyApp extends StatelessWidget {
       title: 'Warehouse',
       home: BottomTabBar(),
     );
+  }
+}
+
+class BottomTabBar extends StatefulWidget {
+  BottomTabBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomTabBar> createState() => _BottomTabBarState();
+}
+
+class _BottomTabBarState extends State<BottomTabBar> {
+  int _index = 0;
+  List<Widget> _pages = [DashboardPage(), WarehousePage(), LocationsPage(), ConfigPage()]; 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Warehouse'),
+        centerTitle: true,
+        backgroundColor: globals.mainColor,
+        elevation: 0,
+      ),
+      body: _pages[_index],
+      bottomNavigationBar: GNav(
+          tabs: [
+            GButton(
+              icon: (_index == 0) ? Icons.house : Icons.house_outlined,
+              text: 'Dashboard',
+            ),
+            GButton(
+              icon: (_index == 1) ? Icons.warehouse : Icons.warehouse_outlined,
+              text: 'Warehouse',
+            ),
+            GButton(
+              icon: (_index == 2) ? Icons.location_on : Icons.location_on_outlined,
+              text: 'Location',
+            ),
+            GButton(
+              icon: (_index == 3) ? Icons.person : Icons.person_outlined,
+              text: 'Profile',
+            )
+          ],
+          selectedIndex: _index,
+          onTabChange: (index) {
+            setState(() {
+              _index = index;
+            });
+          },
+          backgroundColor: globals.mainColor),
+      );
   }
 }
